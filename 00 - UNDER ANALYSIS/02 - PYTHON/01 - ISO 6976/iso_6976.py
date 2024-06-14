e0f26dc94dd8195979681b8c84866fb9e75c6fdd
Pablo Barral <pbarral@fi.uba.ar>
2024-06-14 12:08:39 -0300

    Update iso_6976.py

--- 00 - UNDER ANALYSIS/02 - PYTHON/01 - ISO 6976/iso_6976.py
+++ 00 - UNDER ANALYSIS/02 - PYTHON/01 - ISO 6976/iso_6976.py
@@ -13,6 +13,13 @@ import pandas as pd
 """
     Std inputs are in the accompaning csv files, except por L_0, R, which are included in this script.
     This script is based on the 2016 version of the ISO 6976 standard. If a new version, this code needs updating.
+
+    A possible upgrade is to interpolate calorific values for t1 values different from the preset ones.
+
+    For the purposes of this script, th is calculation (formula (11)) is only valid for Z > 0 ,9.
+
+    Formula (1) -Z(t2, p2)- can also be used to calculate values of compression factor of pure components , but this will not necessarily give the most accurate result possible.
+    In particular, the formula will not provide a value for the compression factor of hydrogen , helium or neon, for which Z > 1, nor for any components, such as the higher hydrocarbons, that are not gaseous at the metering reference conditions. The user should consider the fitness-for-purpose of any such calculation before its use outside of the context of the script.
 """
 # -----------------------------------------
 
@@ -27,6 +34,8 @@ p_0 = 101.325 # kPa(a)
 
     # 3.11 Combustion reference conditions
     #   Specified temperature , t1, and pressure, p1, at which the fuel is notionally burned. In calorific value calculations, products of combustion are at the same temperature than gas + oxygen.
+    #   t_1 can only belong to a predefined set of values (0 C, 15 C, 15.55 C (meaning 60 F), 20 C, 25 C).
+
 
 t_1 = 25 # C
 p_1 = 101.325 # kPa(a)
@@ -34,6 +43,10 @@ p_1 = 101.325 # kPa(a)
     # 3.12 Metering reference conditions
     #   Specified temperature , t2, and pressure, p2, at which the volume of fuel to be burned is notionally determined. 
     #   Note 1 to entry: There is no a priori reason for the metering reference conditions to be the same as the combustion reference conditions (see Figure 1) .
+    #   t_2 can only belong to a predefined set of values (0 C, 15 C, 15.55 C (meaning 60 F), 20 C).
+
+    # The summation is taken over all N components of the mixture and the formula is valid for the range 90 < p2/kPa < 110.
+
 
 t_2 = 15 # C
 p_2 = 101.325 # kPa(a)
@@ -95,17 +108,36 @@ for file, columns in files_and_columns.items():
 # Acessing arrays
 x_j = arrays['molar_fraction.csv']['x_j'] # Array. User input. csv file: component_j & x_j.
 
+if t_2 == 0:
+    s_j_0 = arrays['summation_factor.csv']['s_j_0'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j.
+    s_j = s_j_0
+elif t_2 == 15:
+    s_j_15 = arrays['summation_factor.csv']['s_j_15'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j.
+    s_j = s_j_15
+elif t_2 == 15.55:
+    s_j_15_55 = arrays['summation_factor.csv']['s_j_15_55'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j.
+    s_j = s_j_15_55
+elif t_2 == 20:
+    s_j_20 = arrays['summation_factor.csv']['s_j_20'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j.
+    s_j = s_j_20
+
+u_s_j = arrays['summation_factor.csv']['u_s_j'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j.
+
+
 M_j = arrays['molar_mass.csv']['M_j'] # Array. Molar mass. Table 1. csv file: component_j & M_j.
 
-a_j = arrays['atomic_index.csv']['a_j'] # Array. Atomic index (CHNOS). Table 1. csv file: component_j & a_j & b_j & c_j & d_j & e_j
-b_j = arrays['atomic_index.csv']['b_j'] # Array. Atomic index (CHNOS). Table 1. csv file: omponent_j & a_j & b_j & c_j & d_j & e_j
-c_j = arrays['atomic_index.csv']['c_j'] # Array. Atomic index (CHNOS). Table 1. csv file: component_j & a_j & b_j & c_j & d_j & e_j
-d_j = arrays['atomic_index.csv']['d_j'] # Array. Atomic index (CHNOS). Table 1. csv file: component_j & a_j & b_j & c_j & d_j & e_j
-e_j = arrays['atomic_index.csv']['e_j'] # Array. Atomic index (CHNOS). Table 1. csv file: component_j & a_j & b_j & c_j & d_j & e_j
-
-s_j_0 = arrays['atomic_index.csv']['a_j'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j
-s_j_15 = arrays['atomic_index.csv']['a_j'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j
-s_j_15_55 = arrays['atomic_index.csv']['a_j'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j
-s_j_20 = arrays['atomic_index.csv']['a_j'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j
-u_s_j = arrays['atomic_index.csv']['a_j'] # Array. Summation factors. Table 2. csv file: component_j & s_j_0 & s_j_15 & s_j_15_55 & s_j_20 & u_s_j
+a_j = arrays['atomic_index.csv']['a_j'] # Array. Atomic index (CHNOS). Table 1. csv file: component_j & a_j & b_j & c_j & d_j & e_j.
+b_j = arrays['atomic_index.csv']['b_j'] # Array. Atomic index (CHNOS). Table 1. csv file: omponent_j & a_j & b_j & c_j & d_j & e_j.
+c_j = arrays['atomic_index.csv']['c_j'] # Array. Atomic index (CHNOS). Table 1. csv file: component_j & a_j & b_j & c_j & d_j & e_j.
+d_j = arrays['atomic_index.csv']['d_j'] # Array. Atomic index (CHNOS). Table 1. csv file: component_j & a_j & b_j & c_j & d_j & e_j.
+e_j = arrays['atomic_index.csv']['e_j'] # Array. Atomic index (CHNOS). Table 1. csv file: component_j & a_j & b_j & c_j & d_j & e_j.
 # -----------------------------------------
+
+
+# -----------------------------------------
+# Formula (1)
+
+Z = 1 - (p_2 / p_0) * (x_j * s_j)**2
+# -----------------------------------------
+print(Z)
+
